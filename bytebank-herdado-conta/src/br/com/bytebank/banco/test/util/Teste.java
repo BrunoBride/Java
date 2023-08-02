2c88eac516ac91d4fe2e1b6e852f46eab27289e1
Bruno Bride <brunexcorp@gmail.com>
2023-08-02 12:14:44 -0300

    refatorando para codigo mais limpo com duas classes anonimas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -44,7 +44,16 @@ public class Teste {
 		lista.add(cc3);
 		lista.add(cc4);
 
-		lista.sort(new Comparator<Conta>() {
+		lista.sort(new Comparator<Conta>() { // classe anonima
+
+			@Override
+			public int compare(Conta c1, Conta c2) {
+				return Integer.compare(c1.getNumero(), c2.getNumero());
+				}
+			}
+		);
+		
+		Comparator<Conta> comp = new Comparator<Conta>() {
 
 			@Override
 			public int compare(Conta c1, Conta c2) {
@@ -52,30 +61,12 @@ public class Teste {
 				String nomeC2 = c2.getTitular().getNome();
 				return nomeC1.compareTo(nomeC2);
 			}
-		});
+		};
 
+		
 		for (Conta conta : lista) {
 			System.out.println(conta + ", " + conta.getTitular().getNome());
 		}
 	}
 }
 
-class NumeroDaContaComparator2 implements Comparator<Conta> {
-
-	/// ????
-
-	@Override
-	public int compare(Conta c1, Conta c2) {
-		return Integer.compare(c1.getNumero(), c2.getNumero());
-	}
-}
-
-class TitularDaContaComparator2 implements Comparator<Conta> {
-
-	@Override
-	public int compare(Conta c1, Conta c2) {
-		String nomeC1 = c1.getTitular().getNome();
-		String nomeC2 = c2.getTitular().getNome();
-		return nomeC1.compareTo(nomeC2);
-	}
-}
